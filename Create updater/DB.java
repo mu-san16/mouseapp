89f5da5d3bc4cbdd71ffb7e72f1d319abeabfd25
Mu- <Mu@gmail.com>
2024-05-18 21:04:14 +0900

    Update DB.java

--- Create updater/DB.java
+++ Create updater/DB.java
@@ -4,3 +4,35 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.SQLException;
 
+public class Mouse {  //Mouseという名前クラスの作成
+    public static void main(String[] argrs) {  //mainメゾットを定義
+        // データベース接続情報
+        String url = "jdbc:mysql://localhost:3306/mouse"
+        String user = "root";
+        String password = "password";
+        Stirng sql = "INSERT INTO mouse VALUES('+/-', '2024/5/1', 1)";  //VALUESの部分は変更が必要です
+
+        Connection con = null;  //データベース接続を保持するためのオブジェクトの宣言
+        Statement stmt = null;  //SQL文を実行するためのオブジェクトの宣言
+
+        try{
+            //DBに接続
+            con = DriverManager.getConnection(url, user, password);
+            //ステートメントの生成
+            stmt = con.createStatement();
+            //SQLを実行
+            int count = stmt.excuteUpdate(sql);
+            System.out.println(count + "件更新しました"); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //リソースを解放
+                if (stmt ! = null) stmt.close();
+                if (con ! = null) con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
