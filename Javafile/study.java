bdc512f632c7f31a7d90a1410cbeb50b2cfedd53
Mu- <Mu@gmail.com>
2024-05-21 15:43:32 +0900

    Study Java

---
+++ Javafile/study.java
@@ -0,0 +1,9 @@
+public class study {
+    public static void main(String args[]) {    //Javaプログラムを実行するために必要な形式的なもの
+         
+         System.out.println("Hello world");    //文字を出力するための方法
+         System.out.println("よろしくお願いします");  //同じ部分にまとめてもいいが改行ができないので注意 ("Hello world" + "よろしくお願いします")
+         System.out.println(5-12);
+    }
+}
+
