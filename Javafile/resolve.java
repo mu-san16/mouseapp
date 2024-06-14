07821cdc53fd33b73720ca4b8120b3ed7b455700
Mu- <Mu@gmail.com>
2024-06-14 13:51:12 +0900

    Update resolve.java

--- Javafile/resolve.java
+++ Javafile/resolve.java
@@ -52,6 +52,13 @@ public class resolve{
             System.out.println(a + "/" + b + "は割り算できません");
         }
         System.out.println("最大値は" + max + "です");
+
+        //問題4解答
+        for (int i = 1; i<10; i++){
+            for (int j =1; j<10; j++){
+                System.out.println(i + "*" + j + "は" + i * j);
+            }
+        } 
     }
 }
 
