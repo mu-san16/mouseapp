f3d02907569e8a1b179c5983cdc02279d318491a
Mu- <Mu@gmail.com>
2024-07-12 18:37:26 +0900

    Update Mouse.java

--- Javafile/Mouse.java
+++ Javafile/Mouse.java
@@ -2,15 +2,15 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-// import java.sql.SQLException;
+import java.sql.SQLException;
 
  //Mouseという名前クラスの作成
 public class Mouse{
     public static void main(String[] argrs) {  //mainメゾットを定義
         // データベース接続情報
-        String url = "jdbc:mysql://localhost:3306/mouse";
+        String url = "jdbc:mysql://localhost:3306/mouseapp";  //mouseappの部分は変更が必要
         String user = "root";
-        String password = "password";
+        String password = "password";  // 変更が必要
         //この上3つのコードに関しては今後書き換えることがないものとなっている
 
         String sql = "INSERT INTO mouse VALUES('+/-', '2024/5/1', 1)";  //VALUESの部分は変更が必要です
@@ -19,6 +19,7 @@ public class Mouse{
         Statement stmt = null;  //SQL文を実行するためのオブジェクトの宣言
 
         try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
             //DBに接続
             con = DriverManager.getConnection(url, user, password);
             //ステートメントの生成
