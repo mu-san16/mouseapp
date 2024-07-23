294630b2d26b451312f1186a8bddceabc748416b
Mu- <Mu@gmail.com>
2024-07-24 01:06:57 +0900

    Create Mouseapp

---
+++ Mouse/WEB-INF/classes/LoginServlet.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet{
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+        throws IOException, ServletException{
+            req.setCharacterEncoding("utf-8");
+            res.setContentType("text/html;charset=utf-8");
+
+            String name = req.getParameter("name");
+            req.getSession().setAttribute("userName", name);
+
+            res.sendRedirect("mouseInfo.html?userName" + name);
+        }
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws IOException, ServletException{
+            doPost(req, res);
+        }
+}
