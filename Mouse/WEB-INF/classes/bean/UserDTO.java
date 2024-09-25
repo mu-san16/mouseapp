d0b658e2c02b80c052be06f2d51547824372fec4
mu-san16 <muap16@gmail.com>
2024-09-25 15:23:40 +0900

    create UserDTO.java

--- Mouse/WEB-INF/classes/bean/UserDTO.java
+++ Mouse/WEB-INF/classes/bean/UserDTO.java
@@ -1,11 +1,13 @@
 // データベースとアプリケーション間でユーザー情報を転送するためのデータ構造を提供
 // データのカプセル化：ユーザー関連のデータをひとまとめにする
 
+//他のファイルからのアクセスをする必要があるので記載する必要がある
 public class UserDTO {
     private int id;
     private String username;
     private String password;
 
+    //この部分は初期化するために必要な部分
     public UserDTO() {}
 
     public UserDTO(int id, String username, String password) {
@@ -13,6 +15,7 @@ public class UserDTO {
         this.username = username;
         this.password = password;
     }
+
     //getとsetは他のファイルとのやりとりで使用することが目的となっている(橋渡し)
     public int getId() {
         return id;
