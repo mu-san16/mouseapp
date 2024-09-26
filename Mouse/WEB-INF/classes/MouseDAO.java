6dd6cf787daa0b2eaec904ce7916de2c5d365e67
mu-san16 <muap16@gmail.com>
2024-09-26 12:49:40 +0900

    create login.jsp

--- Mouse/WEB-INF/classes/MouseDAO.java
+++ Mouse/WEB-INF/classes/MouseDAO.java
@@ -1,1 +1,31 @@
 // マウス情報のデータベース操作（挿入、取得、更新、削除）を担当
+import java.spl.*;
+
+public class UserDAO {
+    //この部分のsqlの設定をしており、sqlでデータベースを作成する必要がある(次タスク)
+    private final String URL = "jdbc:mysql://localhost/xxxxxxx"
+    private final String USER = "root"
+    private final String PASS = "pass"
+    private Connection con = null;
+
+    public void connect() {
+        try{
+            con = DriverManager.getConnection(URL, USER, PASS);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //public String selectの部分を作り上げる必要がある
+    public String select(xxx){
+
+    }
+    public void disconnect () {
+        try{
+            //DBを切断
+            if(con != null) con.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- Mouse/WEB-INF/classes/UserDAO.java
+++ Mouse/WEB-INF/classes/UserDAO.java
@@ -1,1 +1,18 @@
 // ユーザー情報のデータベース操作（挿入、取得、更新、削除）を担当
+import java.spl.*;
+
+public class UserDAO {
+    //この部分のsqlの設定をしており、sqlでデータベースを作成する必要がある(次タスク)
+    private final String URL = "jdbc:mysql://localhost/xxxxxxx"
+    private final String USER = "root"
+    private final String PASS = "pass"
+    private Connection con = null;
+
+    public void connect() {
+        try{
+            con = DriverManager.getConnection(URL, USER, PASS);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
