beceb9cf842c2004743e3cb97fe508386ac1c8ef
mu-san16 <muap16@gmail.com>
2024-09-26 13:09:15 +0900

    create UserDAO.java

--- Mouse/WEB-INF/classes/UserDAO.java
+++ Mouse/WEB-INF/classes/UserDAO.java
@@ -1,18 +1,30 @@
-// ユーザー情報のデータベース操作（挿入、取得、更新、削除）を担当
-import java.spl.*;
+//データベースとプログラムの橋渡しをする重要なファイル
+//1. 通訳者の役割、2. 管理人の役割、 3.セキュリティ担当の役割などを持っている
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 public class UserDAO {
-    //この部分のsqlの設定をしており、sqlでデータベースを作成する必要がある(次タスク)
-    private final String URL = "jdbc:mysql://localhost/xxxxxxx"
-    private final String USER = "root"
-    private final String PASS = "pass"
-    private Connection con = null;
+    private DataSource dataSource;
 
-    public void connect() {
-        try{
-            con = DriverManager.getConnection(URL, USER, PASS);
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+    public UserDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    //ユーザーのログイン認証を行う (3.セキュリティ担当の役割が一番近い)
+    public void authenticateUser(String username, String password) throws SQLException {
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, username);
+                pstmt.setString(2, password);
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    return rs.next();
+                }
+            }
     }
 }
